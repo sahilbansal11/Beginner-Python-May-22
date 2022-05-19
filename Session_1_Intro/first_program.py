92423dc4f7d9352007deda0e119be164ef7e8683
Sahil <67091866+sahilbansal11@users.noreply.github.com>
2022-05-19 09:32:07 +0530

    Create first_program.py

---
+++ Session_1_Intro/first_program.py
@@ -0,0 +1,19 @@
+# Comments: some explanation of your code
+
+# Anything after # is a comment, in the same line
+
+# string = collection of characters
+# print("Hello World")
+
+# this string gets printed in the new line
+# print('Hello Scalerites')
+
+# learn taking input from the user
+
+# x = "Sahil"
+
+x = input()
+y = input()
+print("Hello")
+print(x)
+print(y)
